5169cfa89e0e958c13030e12c60d0bbc768d8bdf
Emmanuel Franco <79014949+Emmanuelf21@users.noreply.github.com>
2024-11-26 21:17:40 -0300

    mouseEvent.js

--- Aula5/exemplos/mouseEvent.js
+++ Aula5/exemplos/mouseEvent.js
@@ -3,15 +3,9 @@ const botao = document.getElementById("botao");
 
 botao.addEventListener("mouseover", () =>{
     botao.style.cursor = 'pointer';
-    if (caixa.classList.contains("oculto")) {
-        // Troca a classe para "oculto"
-        caixa.classList.replace("oculto", "visivel");
-    }
+    caixa.classList.replace("oculto", "visivel");
 });
 
 botao.addEventListener("mouseout", ()=>{
-    if (caixa.classList.contains("visivel")) {
-        // Troca a classe para "oculto"
-        caixa.classList.replace("visivel", "oculto");
-    }
+    caixa.classList.replace("visivel", "oculto");
 });
