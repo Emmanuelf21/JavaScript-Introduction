13fd454043638f583d416831a4d6f31008901d67
Emmanuel Franco <79014949+Emmanuelf21@users.noreply.github.com>
2024-12-03 22:06:23 -0300

    Update script2.js

--- extra/cadastrar_dados/script2.js
+++ extra/cadastrar_dados/script2.js
@@ -7,7 +7,8 @@ btnCadastrar.addEventListener("click", () => cadastrarDados());
 function cadastrarDados() {
   let evento = document.querySelector("#evento").value;
   let preco = document.querySelector("#preco").value;
-  array.push({ 'id': id, 'evento': evento, 'preco': preco });
+  array.push({ id: id, evento: evento, preco: preco });
+  id++;
   add();
 }
 
@@ -17,24 +18,39 @@ function add() {
   let tdEvento = document.createElement("td");
   let tdPreco = document.createElement("td");
   let tdBtns = document.createElement("td");
-  
+  let pos = array.length - 1;
+
   let btnEditar = document.createElement("button");
   btnEditar.classList.toggle("editar");
   btnEditar.innerText = "Editar";
+  btnEditar.id = array[pos].id;
 
   let btnExcluir = document.createElement("button");
   btnExcluir.classList.toggle("excluir");
-  btnExcluir.innerText = 'Excluir';
+  btnExcluir.innerText = "Excluir";
+  btnExcluir.id = array[pos].id;
 
-  for(let i=0; i<array.length; i++){
-    tdId.innerText = array[i].id;
-    tdEvento.innerText = array[i].evento;
-    tdPreco.innerText = array[i].preco;
-    tdBtns.append(btnEditar, btnExcluir);
-  
-    tr.append(tdId, tdEvento, tdPreco, tdBtns);
-  }
+  btnEditar.addEventListener("click", () => editarDados(btnEditar.id));
+  btnExcluir.addEventListener("click", () => excluirDados(btnExcluir.id));
+
+  tdId.innerText = array[pos].id;
+  tdEvento.innerText = array[pos].evento;
+  tdPreco.innerText = array[pos].preco;
+  tdBtns.append(btnEditar, btnExcluir);
+
+  tr.append(tdId, tdEvento, tdPreco, tdBtns);
 
   tabela.appendChild(tr);
 }
 
+function editarDados() {}
+
+function excluirDados(idRecebido) {
+  for (let i = 0; i < array.length; i++) {
+    if (idRecebido == array[i].id) {
+      array.splice(i, 1);
+      break;
+    }
+  }
+  gerarTabela();
+}
